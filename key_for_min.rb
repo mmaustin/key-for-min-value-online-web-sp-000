e96d107b5f1c0b08718755d7fdf60cd3f5dc0c1d
mccray austin <tubach75@hotmail.com>
2020-09-27 14:53:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@
 
 def key_for_min_value(names_hash)
   target = 500
+  host = ""
   names_hash.collect do |name, value|
     if value < target
       target = value
-      name
+      host = name
     end
   end
+  host
 end
 key_for_min_value({blake: 500, ashley: 2, adam: 1})
